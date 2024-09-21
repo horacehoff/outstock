0e6b694468fdd4def12d8c22a8bd3133b8ca9f35
just_a_mango <horace.hoff@gmail.com>
2024-09-21 02:25:15 +0200

    /

--- backend/converter.py
+++ backend/converter.py
@@ -1,5 +1,5 @@
 def file_to_messages(filepath):
-    def read_in_chunks(file_object, chunk_size=10000000):
+    def read_in_chunks(file_object, chunk_size=10450000):
         while True:
             data = file_object.read(chunk_size)
             if not data:
