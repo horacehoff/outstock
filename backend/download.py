27b068d7890548e4333bf9ad3fe7b35419b7a17c
just_a_mango <horace.hoff@gmail.com>
2024-09-21 15:49:51 +0200

    /

--- backend/download.py
+++ backend/download.py
@@ -9,17 +9,16 @@ from backend.converter import fragments_to_file
 def download(url: str, fname: str, chunk_size=1024, total_files=10):
     resp = requests.get(url, stream=True)
     total = int(resp.headers.get('content-length', 0))
-    with open("temp/"+fname, 'wb') as file, tqdm(
-        desc=fname.replace("fragment","fragment ")+f"/{total_files}",
-        total=total,
-        unit='iB',
-        unit_scale=True,
-        unit_divisor=1024,
+    with open("temp/" + fname, 'wb') as file, tqdm(
+            desc=fname.replace("fragment", "fragment ") + f"/{total_files}",
+            total=total,
+            unit='iB',
+            unit_scale=True,
+            unit_divisor=1024,
     ) as bar:
         for data in resp.iter_content(chunk_size=chunk_size):
             size = file.write(data)
             bar.update(size)
-
 
 history = {}
 with open("../history", "r") as f:
@@ -28,17 +27,16 @@ with open("../history", "r") as f:
     except:
         print("Please first upload some files!")
 
-print(list(history.keys()))
-
 def download_file(filename):
     if not os.path.exists("temp/"):
         os.makedirs("temp/")
     i = 0
     for url in history[filename]:
-        download(url, "fragment"+str(i+1), total_files=len(history[filename]))
+        download(url, "fragment" + str(i + 1), total_files=len(history[filename]))
         i += 1
     fragments_to_file("temp/", filename.split("-**-")[0])
     rmtree("temp/")
 
 
-download_file("PIA22418_upscayl_4x_ultrasharp.png-**-1726925343726")
+def retrieve_downloads():
+    return [(x.split("-**-")[0], x.split("-**-")[1]) for x in list(history.keys())]
