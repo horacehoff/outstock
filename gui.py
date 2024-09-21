f036a68c311d6bc0a6a8461363455bf664a7cf7b
Anosk <ulysse.coispellier@gmail.com>
2024-09-21 15:43:49 +0200

    blabla

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
+import customtkinter as ctk
 from PIL import Image, ImageTk
 
 from backend.webhook import upload_file
@@ -9,21 +11,59 @@ def fileSel():
     file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("All files", "*.*")])
     if file_path:
         upload_file(file_path)
-        buttonTest.config(text=f"Selected File: {file_path}")
+        fileSelector.config(text=f"Selected File: {file_path}")
         return file_path
 
-root = Tk()
-root.wm_title("Outstock")
+def help():
+    help = Toplevel()
+    help.title("Help")
+    help.geometry("500x200")
+    help.resizable(False, False)
+    help.config(bg="white")
+
+    helpLabel = Label(help, text="Welcome to OutStock", font=("Arial", 12), bg="white")
+    helpLabel.pack()
+
+    helpLabel2 = Label(help, text="", font=("Arial", 12), bg="white")
+    helpLabel2.pack()
+
+    helpLabel3 = Label(help, text="This tool is used to save your files on the discord servers.", font=("Arial", 12), bg="white")
+    helpLabel3.pack()
+
+    helpLabel4 = Label(help, text="Our technology takes your file, breaks it down in sendable packets and then", font=("Arial", 12), bg="white")
+    helpLabel4.pack()
+
+    helpLabel5 = Label(help, text="recompiles them when needed", font=("Arial", 12), bg="white")
+    helpLabel5.pack()
+
+    helpLabel6 = Label(help, text="", font=("Arial", 12), bg="white")
+    helpLabel6.pack()
+
+    helpLabel7 = Label(help, text="", font=("Arial", 12), bg="white")
+    helpLabel7.pack()
+
+    helpLabel8 = Label(help, text="", font=("Arial", 12), bg="white")
+    helpLabel8.pack()
+
+    helpLabel9 = Label(help, text="", font=("Arial", 12), bg="white")
+    helpLabel9.pack()
+
+root = ctk.CTk()
 
 ico = Image.open('guiAssets/outstock.ico')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
 
+help = ctk.CTkButton(master=root, width=120, height=32, border_width=0, corner_radius=8, text="Help ?", command=help)
+help.place(x=0.5, y=0.5, anchor=NW)
 
-label = Label(root, text="Hello World")
-buttonTest = Button(root, text="Choose File", command=fileSel)
+fileSelector = ctk.CTkButton(master=root, width=120, height=32, border_width=0, corner_radius=8, text="Select File", command=fileSel)
+fileSelector.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-buttonTest.pack()
-label.pack()
+root.title("OutStocker - Sending")
+root.geometry("500x500")
+
+
+
 
 root.mainloop()
