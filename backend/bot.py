55ff24eaaec1cb838880d715a982d43d96c447fd
just_a_mango <horace.hoff@gmail.com>
2024-09-21 02:58:00 +0200

    /

--- backend/bot.py
+++ backend/bot.py
@@ -2,21 +2,36 @@ import discord
 import os
 from discord.ext import tasks
 from backend.converter import file_to_messages
+from discord.ext import commands
+
+
 
 
 
 class BotClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
-        self.printer.start()
+        self.scanner.start()
+
+
+    async def on_message(self, message):
+        messages = [message async for message in client.get_channel(1246476145176870924).history()]
+        messages_names = []
+        for message in messages:
+            if not "SENDING" in message.content:
+                messages_names.append(message.content.split("+-/+")[0])
+            await message.delete()
+        messages_names = list(set(messages_names))
+        print(messages_names)
+        print(messages)
+
+
 
     @tasks.loop(seconds=1.0)
-    async def printer(self):
+    async def scanner(self):
         with open("threads", 'r') as f:
             if f.read() != "":
                 f.seek(0)
-                # print(f.read())
-                # f.close()
                 filepath = f.read()
                 f.close()
                 with open("threads","w") as g:
@@ -47,6 +62,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = BotClient(intents=intents)
 
-
 def start():
     client.run('MTI4NjcyODIyNDk2OTE5OTc1Ng.GDN89x.0ocHVNaJC7r31sWNxTlk3zqltLjTxbYNBsPMgc')
+
+if __name__ == "__main__":
+    start()
